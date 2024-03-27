e3d3866d864bc629aa258af88d7f3eb210c56f90
Peacechimamaka <Peaceamaka111@gmail.com>
2024-03-27 23:34:09 +0100

    16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,8 +1,7 @@
 #include "binary_trees.h"
 
 const binary_tree_t *get_leaf(const binary_tree_t *tree);
-int is_perfect_recursive(const binary_tree_t *tree, 
-size_t leaf_depth, size_t level);
+int is_perfect_recursive(const binary_tree_t *tree, 									size_t leaf_depth, size_t level);
 unsigned char is_leaf(const binary_tree_t *node);
 size_t depth(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
